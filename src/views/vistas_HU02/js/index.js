f6dd6b02d09204d7c45aa4cdb4e5a373c2411cff
marlon <marlon.mosquerar@gmail.com>
2024-11-15 16:56:28 -0500

    se creo la carpeta data y añadio la funcionalidad para el boton de enviar datos, falta añadir la que pagina hace la redireccion

--- src/views/vistas_HU02/js/index.js
+++ src/views/vistas_HU02/js/index.js
@@ -1,4 +1,5 @@
-import { usuarios,CrearOpciones,MostrarBeneficioPoliza } from "./mostrarPolizas.js";
+import {CrearOpciones,MostrarBeneficioPoliza } from "./mostrarPolizas.js";
+import { usuarios } from "../../../data/usuarios.js";
 
 const selectPoliza = document.getElementById('polizas');
 const selectBeneficio = document.getElementById('beneficios');
@@ -10,6 +11,7 @@ const areaTrabajo = document.getElementById('areaTrabajo')
 const numeroCelular = document.getElementById('numeroCelular')
 const correo = document.getElementById('correo')
 const ciudad = document.getElementById('ciudad')
+const btnEnviar = document.getElementById('btn-enviar')
 
 
 
@@ -30,4 +32,26 @@ selectPoliza.addEventListener('change',(e) => {
     const nombrePoliza = e.target.value
     MostrarBeneficioPoliza(nombrePoliza,selectBeneficio)
   
+});
+
+btnEnviar.addEventListener('click',() => {
+    // poner redireccion
+      Swal.fire({
+        title: "Benefico",
+        text: "Ha Seleccionado su Benefico Correctamente",
+        icon: "success",
+        confirmButtonText: "Ok"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Llama a tu función personalizada o redirecciona directamente
+          redireccionarPagina();
+        }
+      });
+      
+      // Definir tu función de redirección personalizada
+      function redireccionarPagina() {
+
+        // poner redireccion
+        window.location.href = "#"; // Cambia esta URL a donde quieras redirigir
+      }
 })
