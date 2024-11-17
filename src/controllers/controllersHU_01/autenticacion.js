192e9d6a85d6f23f93b2bad64e44173e5838e4eb
Juan Villa <jujotovil@gmail.com>
2024-11-16 21:35:28 -0500

    implementing first version of autentication

--- src/controllers/controllersHU_01/autenticacion.js
+++ src/controllers/controllersHU_01/autenticacion.js
@@ -1,12 +1,14 @@
 import {usuarios} from '../../data/usuarios.js'
 
 export function sesion(usuarioValidado){
-      localStorage.setItem('usuarioValidado', JSON.stringify(usuarioValidado))
+      sessionStorage.setItem('usuarioValidado', JSON.stringify(usuarioValidado))
+      
+      console.log("dentro de sesion");
 
-      if(usuarioValidado.rol === 'administrador')
-            window.location.href = '';
+      if(usuarioValidado.rol === 'admin')
+            window.location.href = '../../views/vistas_HU01/prueba.html';
       else if(usuarioValidado.rol === 'empleado'){
-            window.location.href = '';
+            window.location.href = '../../views/vistas_HU01/prueba2.html';
       }
 }
 
@@ -16,9 +18,22 @@ const botonInicioSesion = document.getElementById('iniciarSesionButton');
 
 botonInicioSesion.addEventListener('click', () => {
 
+      const regexNumeros = /^[0-9]+$/;
+
       const inputTipoIdentificacionUsuario = document.getElementById('identificacionUsuarion').value;
       const inputNumeroUsuario = document.getElementById('numeroIdentificacionUsuario').value;
       const inputContraseñaUsuario = document.getElementById('contraseñaUsuario').value;
+      
+      let usuarioExistente = usuarios.find(usuario => String(usuario.tipoDocumento).toUpperCase() === inputTipoIdentificacionUsuario && usuario.numeroDocumento === inputNumeroUsuario && usuario.contraseña === inputContraseñaUsuario );
 
-      console.log(inputTipoIdentificacionUsuario);
+      if (usuarioExistente && regexNumeros.test(inputNumeroUsuario)){
+
+            sesion(usuarioExistente)
+
+      } else {
+
+            alert('Error')
+      }
+      
+      
 })
