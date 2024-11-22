3c28747f62dfc437cb1cb3532b92050b5b51b3ba
Luis-Ospino <luisospinodev@gmail.com>
2024-11-22 11:40:48 -0500

    validación para la selección de proveedores, polixas y beneficios

--- src/views/vistas_HU05/js/validacionesAdmon.js
+++ src/views/vistas_HU05/js/validacionesAdmon.js
@@ -1,7 +1,34 @@
 const agregar = document.getElementById('btnAgregar');
 
 agregar.addEventListener("click", function(event){
+    
+    let selectorProveedor = document.getElementById('selectorProveedor');
+    let selectorPolizasActivas = document.getElementById('selector-polizas-activas');
+    let selectorPolizasDisponibles = document.getElementById('selector-polizas-disponibles');
+    let selectorBeneficios = document.getElementById('selector-beneficios');
+
     event.preventDefault();
 
-    
+    if(selectorProveedor.value === "0"){
+        alert('Seleccione un proveedor');
+        return;
+    }
+
+    if(selectorPolizasActivas.value === "0"){
+        alert('Seleccione una poliza activa');
+        return;
+    }
+
+    if(selectorPolizasDisponibles.value === "0"){
+        alert('Seleccione una póliza disponible');
+        return;
+    }
+
+    if(selectorBeneficios.value === "0"){
+        alert('Seleccione un beneficio');
+        return;
+    }
+
+    alert(`su provedor es: ${selectorProveedor.value}, su póliza activa es: ${selectorPolizasActivas.value}, la dispobible es: ${selectorPolizasDisponibles.value}, su beneficio es: ${selectorBeneficios.value}`)
+
 })
