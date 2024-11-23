78217ef7bbb4bcf28ecc9c3a930a909ea1babc2f
Luis-Ospino <luisospinodev@gmail.com>
2024-11-22 23:05:50 -0500

    maquetación de data y controladores

--- src/views/vistas_HU05/js/controlador.js
+++ src/views/vistas_HU05/js/controlador.js
@@ -1,4 +1,3 @@
 import { proveedores } from "../../../data/proveedores.js";
-import { polizasActivas } from "../../../data/polizasActivas.js";
-import { polizasDisponibles } from "../../../data/polizasDisponibles.js";
+
 
