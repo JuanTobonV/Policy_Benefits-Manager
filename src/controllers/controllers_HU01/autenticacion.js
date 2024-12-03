b5538c0d31844a92077cc56406a165a77fc1eab6
Juan Villa <jujotovil@gmail.com>
2024-12-03 16:40:50 -0500

    trying implement autentication

---
+++ src/controllers/controllers_HU01/autenticacion.js
@@ -0,0 +1,48 @@
+import { consultarEmpleados } from "../../sevice/service_HU01/service__HU01.js";
+
+
+
+    const botonInicioSesion = document.getElementById('iniciarSesionButton');
+
+    
+    botonInicioSesion.addEventListener('click', () => {
+
+        const URL = "http://localhost:8080/api/empleado"
+
+        consultarEmpleados(URL).then((respuestaBack) => {
+        
+        const inputTipoIdentificacionUsuario = document.getElementById('identificacionUsuarion').value;
+        const inputNumeroUsuario = document.getElementById('numeroIdentificacionUsuario').value;
+        
+
+        let usuarioExistente = respuestaBack.find(usuario => 
+            String(usuario.tipoIdentificacion).toUpperCase() === inputTipoIdentificacionUsuario && 
+            usuario.numeroIdentificacion == inputNumeroUsuario
+        );
+
+        if (usuarioExistente) {
+            console.log(usuarioExistente);
+            sesion(usuarioExistente);
+        }
+        else{
+            Swal.fire({
+                title: "Usuario no encontrado",
+                icon: "error"
+              });
+        }
+    });
+})
+
+export function sesion(usuarioValidado) {
+    
+    const userId = usuarioValidado.id;
+
+    if (String(usuarioValidado.rol).toUpperCase() === 'ADMIN') {
+        window.location.href = `http://localhost:8080/empleados/${userId}/polizas`;
+    } else if (String(usuarioValidado.rol).toUpperCase() === 'EMPLEADO') {
+        window.location.href = '../../views/vistas_HU02/verPolizasYBeneficios.html';
+    }
+}
+
+
+
