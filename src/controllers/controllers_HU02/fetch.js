4b29551870dac63747193185620a318cd8b1bf76
marlon <marlon.mosquerar@gmail.com>
2024-12-06 18:52:45 -0500

    Implementacion de servicio y modificaciones : 1. se realizo el servicio de crear solicitudes, para permiter al usuario crear una solicitud de la poliza y benecio seleccionado y enviarla al administrador, 2. se hice la redirecion de la pagina selecciona tu solicitud haci la pagina ver mis solicitudes

--- src/controllers/controllers_HU02/fetch.js
+++ src/controllers/controllers_HU02/fetch.js
@@ -1,4 +1,4 @@
-export async function FetchData (url){
+export async function ObtnerPolizasYBeneficos (url){
     const option = {
         method:"GET"
     }
@@ -7,4 +7,23 @@ export async function FetchData (url){
     const data = await response.json()
 
     return data
+}
+
+export async function EnviarSolicitud(url,data) {
+    try {
+        const option = {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(data)
+        }
+        
+        const response = await fetch(url,option)
+        const responseData = await response.json()
+    
+       console.log(responseData)
+    } catch (error) {
+        console.error(error)
+    }
 }
